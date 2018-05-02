c049adfcaa363c408ba31a54876a825e30a0b590
pysty1995 <pysty1995@gmail.com>
2018-05-02 21:21:57 +0700

    Update PIC & 74HC165.c

--- PIC & 74HC165.c
+++ PIC & 74HC165.c
@@ -1,16 +1,4 @@
 /******************************************************************************                                                   
-Chip type           : PIC16F877A         
-Program type        : Application
-Clock frequency     : 20 MHz
-Project             : Mo rong dau vao Input
-Version             : V1.0
-Date                : 03/06/2013
-Author              : DAO XUAN TRUONG 
-Gmail               : daotruongpro@gmail.com   
-Mobile              : 0979.20.90.10
-Company             : JVN Technology., JSC 
-
-Comments : Chuong trinh viet mo rong dau vao Input dung 74HC165
 *******************************************************************************/
 #include <PIC & 74HC165.h>
 #include <74165.c>
